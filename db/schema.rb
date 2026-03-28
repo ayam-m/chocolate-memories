9625507c831d2da9b971cc49f5394ab41121788d
ayam-m <enana.ayami@gmail.com>
2026-03-28 23:45:40 +0900

    add: 記録登録用モデル(テーブル)作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_25_135556) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_28_135507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,36 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_25_135556) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "brand_id"
+    t.integer "record_type", null: false
+    t.date "event_date", null: false
+    t.string "recipient_name"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_records_on_brand_id"
+    t.index ["record_type"], name: "index_records_on_record_type"
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
+
+  create_table "tastings", force: :cascade do |t|
+    t.bigint "record_id", null: false
+    t.integer "sweetness"
+    t.integer "richness"
+    t.integer "melting"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_id"], name: "index_tastings_on_record_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,4 +86,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_25_135556) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "records", "brands"
+  add_foreign_key "records", "users"
+  add_foreign_key "tastings", "records"
 end
