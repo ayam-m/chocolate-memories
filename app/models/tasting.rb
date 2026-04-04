a8404418f08e61f883c8742af213b2b9b3a61017
ayam-m <enana.ayami@gmail.com>
2026-04-04 22:12:04 +0900

    fix: tasting項目をテキスト表示させる記述(登録画面にも反映)

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -1,3 +1,28 @@
 class Tasting < ApplicationRecord
   belongs_to :record
+
+  # showページの味覚評価をtextで表示
+  SWEETNESS_OPTS = {
+    1 => "甘い",
+    2 => "やや甘い",
+    3 => "普通",
+    4 => "ややビター",
+    5 => "ビター"
+  }.freeze
+
+  RICHNESS_OPTS = {
+    1 => "濃厚",
+    2 => "やや濃厚",
+    3 => "普通",
+    4 => "ややあっさり",
+    5 => "あっさり"
+  }.freeze
+
+  MELTING_OPTS = {
+    1 => "なめらか",
+    2 => "ややなめらか",
+    3 => "普通",
+    4 => "やや固い",
+    5 => "固い"
+  }.freeze
 end
