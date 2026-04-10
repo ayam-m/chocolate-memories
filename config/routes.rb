a0e2ad7138010bc3169f008b1c0ccb5d96dfc345
ayam-m <enana.ayami@gmail.com>
2026-04-10 21:43:24 +0900

    fix: 編集ページの動線追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
   root "records#index"
-  resources :records, only: %i[index new create show]
+  resources :records, only: %i[index new create show edit update]
 end
